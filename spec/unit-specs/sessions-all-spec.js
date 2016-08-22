abe037996768f233a928c8ee019491938503a3ec
wyb1995 <wy_b1995@163.com>
2016-08-22 10:40:07 +0800

    jasmine fix

--- spec/unit-specs/sessions-all-spec.js
+++ spec/unit-specs/sessions-all-spec.js
@@ -1,42 +1,28 @@
+'use strict';
 import request from 'supertest';
 import app from '../../app/server';
 import finish from '../finish';
-import {User} from '../../app/mongodb/schema';
-import db from '../../app/mongodb/db';
 import async from 'async';
 
 describe('sessions-spec', () => {
-
-  beforeEach((done)=> {
-    async.series([
-      (cb) => db.connect('test', cb),
-      (cb) => User.find().remove(cb)
-    ], finish(done));
-  });
-
-  afterEach((done)=> {
-    db.close(finish(done));
-  });
-  it('用户输入的ID与密码全都正确，并且不是第一次登录：', (done) => {
-    async.waterfall([
-      (cb) => new User({userId: 's03134054', password: '123456', email: '565678150@qq.com'}).save((err) => {
-        if (err) return done.fail(err);
-        request(app).post('/api/sessions').send({userId: 's03134054', password: '123456'})
-          .expect(201, {message: 'SUCCESS', newUser: false}, cb);
-      })
-    ], finish(done));
-  });
-
-  it('用户输入的ID与密码全都正确，并且是第一次登录：', (done) => {
-    async.waterfall([
-      (cb) => request(app).post('/api/sessions').send({userId: 's03134053', password: '123456'})
-        .expect(201, {message: 'SUCCESS', newUser: true}, cb)
-    ], finish(done));
-  });
-  it('用户输入的用户名密码不匹配：', (done) => {
-    async.waterfall([
-      (cb) => request(app).post('/api/sessions').send({userId: 's03134054', password: '122456'})
-        .expect(401, '用户名或密码有误，登录失败', cb)
-    ], finish(done));
+  describe('Bad Request', ()=> {
+    it('用户未输入用户名：', (done) => {
+      async.waterfall([
+        (cb) => request(app).post('/api/sessions').send({userId: '', password: '123456'})
+          .expect(400, '数据不能为空', cb)
+      ], finish(done));
+    });
+    it('用户未输入密码：', (done) => {
+      async.waterfall([
+        (cb) => request(app).post('/api/sessions').send({userId: 's03134054', password: ''})
+          .expect(400, '数据不能为空', cb)
+      ], finish(done));
+    });
+    it('用户未输入用户名和密码：', (done) => {
+      async.waterfall([
+        (cb) => request(app).post('/api/sessions').send({})
+          .expect(400, '数据不能为空', cb)
+      ], finish(done));
+    });
   });
 });
